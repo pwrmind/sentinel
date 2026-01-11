f59bd1a83d92dccd5d1f1dcca8d542b4c8f4102e
babanov.a <babanov.a@skbkontur.ru>
2026-01-11 21:02:43 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 use std::ptr::null_mut;
-use std::ffi::c_void;
 use std::io::{self, Write};
 use ntapi::ntzwapi::{ZwQuerySystemInformation, ZwOpenProcess, ZwTerminateProcess, ZwReadVirtualMemory};
 use ntapi::ntexapi::{SystemProcessInformation, SYSTEM_PROCESS_INFORMATION};
-use ntapi::ntobapi::OBJECT_ATTRIBUTES;
-use ntapi::ntapi_base::CLIENT_ID;
+use ntapi::ntapi_base::{OBJECT_ATTRIBUTES, CLIENT_ID};
 use windows::Win32::Foundation::{STATUS_SUCCESS, HANDLE};
 use windows::Win32::System::Threading::{PROCESS_TERMINATE, PROCESS_VM_READ, PROCESS_QUERY_INFORMATION};
 use windows::Win32::NetworkManagement::IpHelper::{GetExtendedTcpTable, TCP_TABLE_OWNER_PID_ALL};
@@ -51,9 +49,12 @@ unsafe fn kill_process(pid: usize) -> bool {
     let mut handle = HANDLE(std::ptr::null_mut());
     let mut obj_attr: OBJECT_ATTRIBUTES = std::mem::zeroed();
     obj_attr.Length = std::mem::size_of::<OBJECT_ATTRIBUTES>() as u32;
-    let mut client_id = CLIENT_ID { UniqueProcess: pid as *mut c_void, UniqueThread: null_mut() };
+    let mut client_id = CLIENT_ID { UniqueProcess: pid as *mut _, UniqueThread: null_mut() };
     
-    if ZwOpenProcess(&mut handle.0 as *mut *mut c_void, PROCESS_TERMINATE.0, &mut obj_attr, &mut client_id) == STATUS_SUCCESS.0 {
+    // Приводим handle.0 к нужному типу указателя
+    let mut handle_ptr = handle.0;
+    if ZwOpenProcess(&mut handle_ptr, PROCESS_TERMINATE.0, &mut obj_attr, &mut client_id) == STATUS_SUCCESS.0 {
+        handle.0 = handle_ptr;
         return ZwTerminateProcess(handle.0, 1) == STATUS_SUCCESS.0;
     }
     false
@@ -63,7 +64,7 @@ unsafe fn deep_scan() {
     let mut buffer_size = 0;
     let _ = ZwQuerySystemInformation(SystemProcessInformation, null_mut(), 0, &mut buffer_size);
     let mut buffer = vec![0u8; buffer_size as usize + 8192];
-    if ZwQuerySystemInformation(SystemProcessInformation, buffer.as_mut_ptr() as *mut c_void, buffer.len() as u32, &mut buffer_size) == STATUS_SUCCESS.0 {
+    if ZwQuerySystemInformation(SystemProcessInformation, buffer.as_mut_ptr() as *mut _, buffer.len() as u32, &mut buffer_size) == STATUS_SUCCESS.0 {
         let mut current_ptr = buffer.as_ptr() as *const SYSTEM_PROCESS_INFORMATION;
         loop {
             let proc_info = &*current_ptr;
@@ -84,7 +85,7 @@ unsafe fn scan_network() {
     let mut table_size = 0;
     let _ = GetExtendedTcpTable(Some(std::ptr::null_mut()), &mut table_size, false, AF_INET.0 as u32, TCP_TABLE_OWNER_PID_ALL, 0);
     let mut buffer = vec![0u8; table_size as usize];
-    if GetExtendedTcpTable(Some(buffer.as_mut_ptr() as *mut c_void), &mut table_size, false, AF_INET.0 as u32, TCP_TABLE_OWNER_PID_ALL, 0) == 0 {
+    if GetExtendedTcpTable(Some(buffer.as_mut_ptr() as *mut _), &mut table_size, false, AF_INET.0 as u32, TCP_TABLE_OWNER_PID_ALL, 0) == 0 {
         let table = &*(buffer.as_ptr() as *const MIB_TCPTABLE_OWNER_PID);
         println!("{:<15} | {:<15} | {:<10}", "Local", "Remote", "PID");
         for i in 0..table.dwNumEntries {
@@ -101,12 +102,15 @@ unsafe fn peek_memory(pid: usize, addr: usize) -> u64 {
     let mut handle = HANDLE(std::ptr::null_mut());
     let mut obj_attr: OBJECT_ATTRIBUTES = std::mem::zeroed();
     obj_attr.Length = std::mem::size_of::<OBJECT_ATTRIBUTES>() as u32;
-    let mut client_id = CLIENT_ID { UniqueProcess: pid as *mut c_void, UniqueThread: null_mut() };
+    let mut client_id = CLIENT_ID { UniqueProcess: pid as *mut _, UniqueThread: null_mut() };
     
-    if ZwOpenProcess(&mut handle.0 as *mut *mut c_void, (PROCESS_VM_READ | PROCESS_QUERY_INFORMATION).0, &mut obj_attr, &mut client_id) == STATUS_SUCCESS.0 {
+    // Приводим handle.0 к нужному типу указателя
+    let mut handle_ptr = handle.0;
+    if ZwOpenProcess(&mut handle_ptr, (PROCESS_VM_READ | PROCESS_QUERY_INFORMATION).0, &mut obj_attr, &mut client_id) == STATUS_SUCCESS.0 {
+        handle.0 = handle_ptr;
         let mut value = 0u64;
         let mut read = 0;
-        ZwReadVirtualMemory(handle.0, addr as *mut c_void, &mut value as *mut _ as *mut c_void, 8, &mut read);
+        ZwReadVirtualMemory(handle.0, addr as *mut _, &mut value as *mut _ as *mut _, 8, &mut read);
         return value;
     }
     0
